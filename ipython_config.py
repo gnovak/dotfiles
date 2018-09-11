b83bca6177224f8da0788f72dc9480eea017034d
Greg Novak <greg.novak@gmail.com>
2018-09-11 12:07:52 -0700

    Change ipython to autoselect backend

--- ipython_config.py
+++ ipython_config.py
@@ -2,7 +2,7 @@
 
 c = get_config()
 c.TerminalInteractiveShell.colors = 'LightBG'
-c.InteractiveShellApp.pylab = 'osx'
+c.InteractiveShellApp.pylab = 'auto'
 c.InteractiveShellApp.extensions = ['autoreload', 'grasp']
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
 
